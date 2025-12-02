f78be3d145c97b8b9fb2e1b7709d8e81456b5119
ADI1GROSS <ag8069767@gmail.com>
2025-12-02 12:01:35 +0200

    Update project

--- tealcrm/__init__.py
+++ tealcrm/__init__.py
@@ -1,3 +1,6 @@
-from .celery import app as celery_app
+try:
+    from .celery import app as celery_app
+except ImportError:
+    celery_app = None  # Celery is optional in local dev / Railway
 
 __all__ = ('celery_app',)
