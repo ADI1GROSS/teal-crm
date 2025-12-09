8500e16f11c32b237a345da0f9a4ac6d2b4143de
ADI1GROSS <ag8069767@gmail.com>
2025-12-09 21:18:40 +0200

    Update model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 import uuid 
-
+from core.storage_backends import MediaStorage
 class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
     invite_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
@@ -9,7 +9,7 @@ class Userprofile(models.Model):
     name = models.CharField(verbose_name="שם", max_length=100, blank=True, null=True)
     email = models.EmailField(verbose_name="אימייל", default='')
     phone = models.CharField(verbose_name="טלפון", max_length=50, default='')
-    regulations_document = models.FileField(upload_to='regulations/',blank=True, null=True)
+    regulations_document = models.FileField(upload_to='regulations/',storage=MediaStorage(),blank=True, null=True)
 
 
     def __str__(self):
