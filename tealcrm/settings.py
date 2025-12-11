2b779a6814a3a7b56db36b744ed10a1b4f2e51ba
ADI1GROSS <ag8069767@gmail.com>
2025-12-11 12:07:43 +0200

    port 465

--- tealcrm/settings.py
+++ tealcrm/settings.py
@@ -186,7 +186,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', '')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+
+# ⬅️ שמור על TLS (כברירת מחדל, 587)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+
+# ⬅️ הוסף תמיכה ב-SSL (עבור פורט 465)
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
